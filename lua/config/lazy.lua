21c7d00230705a0bf24bace656c858f23198eb91
VLeon-B <vleonbar.dev@gmail.com>
2024-07-18 17:46:29 +0200

    Little structural changes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,8 +12,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
-		{ import = "plugins.lsp" }
-	}
+  spec = {
+    { import = "plugins" },
+  },
 })
